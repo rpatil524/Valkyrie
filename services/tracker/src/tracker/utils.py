33e37e4486b8d790cfa1198f873338e8d3d758ce
Jarett <jarett@vals.ai>
2026-04-03 23:31:07 -0400

    Track when the agent has timed out (#228)
    * Handle agent timeout restrictions better
    
    * Created tests to test timeout behavior
    
    * Attach timeout to the eval results and remove migration
    
    * Add agent timeout to test object

--- services/tracker/src/tracker/utils.py
+++ services/tracker/src/tracker/utils.py
@@ -377,7 +377,7 @@ async def process_task(
                     agent_output_s3_key = get_agent_result_s3_key(str(benchmark_id), task_id, "agent_output.tar.gz")
 
                 # Run the agent inside of the sandbox
-                await run_agent(
+                agent_timed_out = await run_agent(
                     sandbox,
                     start_benchmark_request.contract,
                     task_data.problem_path,
@@ -406,8 +406,9 @@ async def process_task(
                 buffer_logs(log_queue, stream_key, harness_config.aws, harness_config.log_group, force_flush=True)
 
                 # Save the evaluation result to the database with the task row
+                # Flag the agent timed out when trying to complete the task (expected)
                 evaluation_result_row = EvaluationResult(
-                    task=task_row.id, instance_id=sandbox.id, result=evaluation_result
+                    task=task_row.id, instance_id=sandbox.id, result=evaluation_result, agent_timed_out=agent_timed_out
                 )
 
                 with Session(bind=engine) as task_session:
@@ -760,6 +761,8 @@ def fetch_evaluation_results(benchmark_id: UUID, session: Session) -> dict[str, 
     evaluation_results: dict[str, dict[str, Any]] = {}
     for evaluation_result, task_id in results:
         result_data = evaluation_result.result
+        # NOTE: We append this because its important for the user to know
+        result_data["agent_timed_out"] = evaluation_result.agent_timed_out
         evaluation_results[task_id] = result_data
 
     return evaluation_results
